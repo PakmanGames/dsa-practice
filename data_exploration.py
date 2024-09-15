e86bcc398cf18b74945e75f65c656129f58fa37e
Andy Pak <im.andy.pak@gmail.com>
2024-09-15 11:36:03 -0400

    Create function to test for lowest MAE based on leaf nodes

--- data_exploration.py
+++ data_exploration.py
@@ -3,6 +3,14 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 
+# calculate mae based on number of leaf nodes
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return (mae)
+
 # get info from csv file
 melbourne_file_path = './melb_data.csv'
 melbourne_data = pd.read_csv(melbourne_file_path)
@@ -22,9 +30,6 @@ X = melbourne_data[melbourne_features]
 # split data to train and validate
 train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=0)
 
-# fit the model
-melbourne_model.fit(train_X, train_y)
-
-# check model accuracy using the validation data
-val_predictions = melbourne_model.predict(val_X)
-print(mean_absolute_error(val_y, val_predictions))
+for max_leaf_nodes in [5, 50, 500, 5000]:
+    my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+    print("Max leaf nodes: %d \t\t Mean Absolute Error: %d" %(max_leaf_nodes, my_mae))
