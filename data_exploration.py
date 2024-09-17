60875b83b13507d80277c27e6493be3423235e1b
Andy Pak <im.andy.pak@gmail.com>
2024-09-16 20:59:36 -0400

    Implement random forest model

--- data_exploration.py
+++ data_exploration.py
@@ -1,11 +1,19 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 
 # calculate mae based on number of leaf nodes
-def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+def get_mae_tree(max_leaf_nodes, train_X, val_X, train_y, val_y):
     model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return (mae)
+
+def get_mae_forest(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = RandomForestRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
     model.fit(train_X, train_y)
     preds_val = model.predict(val_X)
     mae = mean_absolute_error(val_y, preds_val)
@@ -16,7 +24,7 @@ melbourne_file_path = './melb_data.csv'
 melbourne_data = pd.read_csv(melbourne_file_path)
 
 # create model
-melbourne_model = DecisionTreeRegressor()
+melbourne_model = RandomForestRegressor(random_state=1)
 
 # drop missing data rows
 melbourne_data = melbourne_data.dropna(axis=0)
@@ -31,5 +39,9 @@ X = melbourne_data[melbourne_features]
 train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=0)
 
 for max_leaf_nodes in [5, 50, 500, 5000]:
-    my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
-    print("Max leaf nodes: %d \t\t Mean Absolute Error: %d" %(max_leaf_nodes, my_mae))
+    my_mae = get_mae_tree(max_leaf_nodes, train_X, val_X, train_y, val_y)
+    print("Max leaf nodes: %d" %(max_leaf_nodes))
+    print("TREE: Mean Absolute Error: %d" %(my_mae))
+    my_forest_mae = get_mae_forest(max_leaf_nodes, train_X, val_X, train_y, val_y)
+    print("FOREST: Mean Absolute Error: %d" %(my_forest_mae))
+    print("\n")
